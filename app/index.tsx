890ee5bca537dd40d4d7df36b1a2422ab5730872
Lucas Garcez <lucasgarcezdev@gmail.com>
2025-10-30 19:31:16 +1100

    comps base

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
+        backgroundColor: "#1B1B1B",
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        // flexDirection: "row",
       }}
     >
-      <Text>my first app</Text>
+      <Text style={styles.title}>Hello world</Text>
+      <Text style={[styles.title, { color: "red" }]}>my first app</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    color: "#FFF",
+    fontSize: 24,
+    fontWeight: 600,
+  },
+});
