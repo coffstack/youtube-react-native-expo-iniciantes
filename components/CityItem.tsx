9e3b03b2aafd010c510319e069e7a2e860972326
Lucas Garcez <lucasgarcezdev@gmail.com>
2025-10-30 19:46:35 +1100

    item style

--- components/CityItem.tsx
+++ components/CityItem.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 import { City } from "@/data/types";
 
@@ -6,13 +6,29 @@ type CityItemProps = {
   city: City;
 };
 export function CityItem({ city }: CityItemProps) {
-  return <Text style={styles.title}>{city.name}</Text>;
+  return (
+    <View style={styles.card}>
+      <Text style={styles.title}>{city.name}</Text>
+      <Text style={styles.description}>{city.description}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  card: {
+    borderWidth: 1,
+    borderColor: "#FF4B4B",
+    // paddingHorizontal: 8,
+    // paddingVertical: 16,
+    padding: 16,
+  },
   title: {
     color: "#FFF",
     fontSize: 24,
     fontWeight: 600,
+  },
+  description: {
+    color: "#FFF",
+    fontSize: 16,
   },
 });
